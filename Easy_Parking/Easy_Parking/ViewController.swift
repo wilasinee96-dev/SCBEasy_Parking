74d68cf3b56cb441e158c8219a253a771638b02f
ZGAME <58160323@go.buu.ac.th>
2019-09-28 15:20:08 +0700

    Create table view

--- Easy_Parking/Easy_Parking/ViewController.swift
+++ Easy_Parking/Easy_Parking/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableViewCarList: UITableView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
