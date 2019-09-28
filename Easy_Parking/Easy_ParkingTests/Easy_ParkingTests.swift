1923ff8c099f85af5279322669b324fad83bc602
ZGAME <58160323@go.buu.ac.th>
2019-09-28 13:57:46 +0700

    Strat Project

---
+++ Easy_Parking/Easy_ParkingTests/Easy_ParkingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Easy_ParkingTests.swift
+//  Easy_ParkingTests
+//
+//  Created by Z64me on 28/9/2562 BE.
+//  Copyright © 2562 Z64me. All rights reserved.
+//
+
+import XCTest
+@testable import Easy_Parking
+
+class Easy_ParkingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
