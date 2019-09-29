24ae3c20528255b7a55e98e49552f30b0c0d3952
Pathomphong Charoenwichianchay <beebbuild@gmail.com>
2019-09-29 22:13:02 +0700

    add SCB Easy Park menu, link to next page, fix navigation bar for hide or show some scene.

--- Easy_Parking/Easy_Parking/ViewController/SCBEasyController.swift
+++ Easy_Parking/Easy_Parking/ViewController/SCBEasyController.swift
@@ -11,4 +11,23 @@ import UIKit
 
 class SCBEasyController: UIViewController {
   
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+
+      // Hide the navigation bar on the this view controller
+      self.navigationController?.setNavigationBarHidden(true, animated: animated)
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+
+      // Show the navigation bar on other view controllers
+      self.navigationController?.setNavigationBarHidden(false, animated: animated)
+  }
+  
 }
