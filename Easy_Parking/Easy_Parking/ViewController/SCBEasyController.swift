5feff6e898bc0911f2c2ea44a8e60c4cae0b6943
Pathomphong Charoenwichianchay <beebbuild@gmail.com>
2019-09-29 20:26:53 +0700

    create SCBEasy home page

---
+++ Easy_Parking/Easy_Parking/ViewController/SCBEasyController.swift
@@ -0,0 +1,14 @@
+//
+//  SCBEasyController.swift
+//  Easy_Parking
+//
+//  Created by pathomphong charoenwichianchay on 29/9/2562 BE.
+//  Copyright © 2562 Z64me. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SCBEasyController: UIViewController {
+  
+}
