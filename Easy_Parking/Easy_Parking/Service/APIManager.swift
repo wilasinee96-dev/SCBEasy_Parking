6958e43a38913f89ffd5279a1161bd4a5ea534c9
Pathomphong Charoenwichianchay <beebbuild@gmail.com>
2019-10-03 23:13:17 +0700

    add podfire , API, Qr controller from rachel

---
+++ Easy_Parking/Easy_Parking/Service/APIManager.swift
@@ -0,0 +1,176 @@
+//
+//  APIManager.swift
+//  Easy_Parking
+//
+//  Created by pathomphong charoenwichianchay on 3/10/2562 BE.
+//  Copyright © 2562 Z64me. All rights reserved.
+//
+
+import Foundation
+enum APIError: Error {
+  case invalidJSON
+  case invalidData
+}
+
+class APIManager {
+    
+    init() {
+        print("API Start .. ")
+         let urlString = "https://api.partners.scb/partners/sandbox/v1/oauth/token"
+              print("url .. "+urlString)
+          guard let urll = URL(string: urlString) else { return }
+           var request = URLRequest(url: urll)
+          request.httpMethod = "POST"
+             
+         let json: [String: Any] = ["applicationKey": "l7f630937ca15d4bb28aeb30a487125dc5",
+                                     "applicationSecret": "91e93c3d86334b5983fe23c899a0e715"]
+        
+        request.setValue("application/json", forHTTPHeaderField:"Content-Type")
+        request.setValue("l7f630937ca15d4bb28aeb30a487125dc5", forHTTPHeaderField:"resourceOwnerId")
+        request.setValue("{{$guid}}", forHTTPHeaderField:"requestUId")
+        request.setValue("EN", forHTTPHeaderField:"accept-language")
+//
+        
+         let jsonData = try? JSONSerialization.data(withJSONObject: json)
+         request.httpBody = jsonData
+             print("TTTTTTEEEEEEESSSSSSSTTTTTTT")
+          let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+              if let error = error {
+                  print("error: \(error)")
+              } else {
+                  if let response = response as? HTTPURLResponse {
+                      print("statusCode: \(response.statusCode)")
+                }
+                 if let data1 = data, let dataString = String(data: data1, encoding: .utf8) {
+                   
+                   let stringJson = (""+dataString+"")
+                    let stringJson2 = "{\"names\": [\"Bob\", \"Tim\", \"Tina\"]}"
+                                       print(stringJson)
+                                       print(stringJson2)
+                                        print("====================================")
+                                       //var jsonString = "[\(dataString)]"
+                                       let data2 = Data(stringJson.utf8)
+                                       let data3 = Data(stringJson2.utf8)
+                    
+                                       print(data2)
+                                       print(data3)
+
+                                                
+             
+                        do {
+                            var entity_guid : Int = 0
+                            var user_guid : Int = 0
+                            let jsonResult = try JSONSerialization.jsonObject(with: data2, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:Any]
+
+                            // Parse JSON data
+                        
+                            
+                            do {
+                                var accessToken = ""
+                                if let jsonP = jsonResult?["data"] as? [String:Any]{
+                                       print(jsonP)
+                                       for jsonitem in jsonP {
+                                        if(jsonitem.key == "accessToken"){
+                                            accessToken = jsonitem.value as! String
+                                            print(  jsonitem.value)
+                                            }
+                                        }
+                                     
+                                    self.requestQR(accesstoken: accessToken)
+                                }
+                                }catch {
+                                    print(error.localizedDescription)
+                                }
+                        
+                        } catch {
+                            
+                            print(error.localizedDescription)
+                        }
+                    
+                     
+                }
+               }
+           }
+        task.resume()
+    }
+  
+
+    private func requestQR(accesstoken: String ) -> Void{
+        print("####################### requestQR #######################")
+        //let urlString2 = "https://api.partners.scb/partners/sandbox/v1/payment/qrcode/create"
+        let url = URL(string: "https://api.partners.scb/partners/sandbox/v1/payment/qrcode/create")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json"    , forHTTPHeaderField:"Content-Type")
+        request.setValue("Bearer "+accesstoken , forHTTPHeaderField:"authorization")
+        request.setValue("l7f630937ca15d4bb28aeb30a487125dc5",forHTTPHeaderField:"resourceOwnerId")
+        request.setValue("{{$guid}}", forHTTPHeaderField:"requestUId")
+        request.setValue("EN", forHTTPHeaderField:"accept-language")
+        
+        let parameters: [String: Any] =
+        [
+            "qrType": "CS",
+            "merchantId": "062541946488497",
+            "terminalId": "047444374717436",
+            "invoice": "INVOICE",
+            "amount": "100.00",
+            "csExtExpiryTime" : "60"
+        ]
+        let jsonData = try? JSONSerialization.data(withJSONObject: parameters)
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    print("statusCode: \(response.statusCode)")
+                }
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("data: \(dataString)")
+                            let stringJsonn = (""+dataString+"")
+                            let dataa = Data(stringJsonn.utf8)
+                          
+                    do {
+                
+
+                            let jsonResult2 = try JSONSerialization.jsonObject(with: dataa, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:Any]
+
+                                // Parse JSON data
+                                           
+                                do {
+                                        var qrImage = ""
+                                        if let jsonP = jsonResult2?["data"] as? [String:Any]{
+                                                print(jsonP)
+                                            for jsonitem in jsonP {
+                                                print("!!!!!!")
+
+                                                if(jsonitem.key == "qrImage"){
+                                                    qrImage = jsonitem.value as! String
+                                                    
+                                                    print(  jsonitem.value)
+                                                }
+
+                                            }
+                                                   }
+                                                   }
+                                           
+                                           } catch {
+                                               
+                                               print(error.localizedDescription)
+                                           }
+                                       
+                }
+            }
+        }
+        task.resume()
+        
+        
+        
+        
+    
+    }
+
+        
+  
+}
--- Easy_Parking/Easy_Parking/ViewController/ViewQRController.swift
+++ Easy_Parking/Easy_Parking/ViewController/ViewQRController.swift
@@ -7,14 +7,114 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewQRController: UIViewController {
 
+    var ref: DatabaseReference!
+
+    var datetime1 = ""
+    var datetime2 = ""
+
+    func getCurrentData1(){
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        formatter.dateFormat = "HH:mm:ss"
+        let str = formatter.string(from: Date())
+        datetime1 = str
+        print(str)
+    }
+    
+    func getCurrentData2(){
+        let formatter2 = DateFormatter()
+        formatter2.timeStyle = .medium
+        formatter2.dateFormat = "HH:mm:ss"
+        let str2 = formatter2.string(from: Date())
+        datetime2 = str2
+        print(str2)
+       }
+    
+    func findDateDiff(time1Str: String, time2Str: String) -> String {
+        let timeformatter = DateFormatter()
+        timeformatter.dateFormat = "HH:mm:ss"
+
+        guard let time1 = timeformatter.date(from: time1Str),
+            let time2 = timeformatter.date(from: time2Str) else { return "" }
+
+        //You can directly use from here if you have two dates
+        print("\(datetime1) + \(datetime2)")
+        let interval = time2.timeIntervalSince(time1)
+        let hour = interval / 3600;
+        let minute = interval.truncatingRemainder(dividingBy: 3600) / 60
+        let intervalInt = Int(interval)
+        print("minute \(minute)")
+        let min = (hour*60)
+        print("min \(min)")
+//        let mins = min+minute
+//        print("mins \(mins)")
+        //return "\(intervalInt < 0 ? "-" : "+") \(Int(hour)) Hours \(Int(minute)) Minutes"
+        return "\(min)"
+    }
+    
+    func getHours(mins: String) -> Int {
+        var hours = 0
+        let sum = (mins as NSString).integerValue
+        if(sum%60 == 0){
+            hours = sum/60
+        }else{
+            hours = (sum/60) + 1
+        }
+        print("price \(hours)")
+        return hours
+    }
+    
+    func getPriceParagon(hours: Int) -> Int {
+        var price = 0
+        let hour = hours-2
+        if(hour==0){
+            price = 0
+        }else if(hour>0 && hours<5){
+            price = hour*30
+        }else if(hour>=5){
+            price = 120
+            let sum = hour-4
+            price = (sum*60) + price
+        }
+        print("\(price)")
+        return price
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getCurrentData1()
+        getCurrentData2()
+        let result = (findDateDiff(time1Str: "10:10:00", time2Str: "13:50:00"))
+        let prices = (getHours(mins: result))
+        getPriceParagon(hours: prices)
         // Do any additional setup after loading the view.
+        self.ref = Database.database().reference()
+           // self.ref.child("AccountNum").child("PlateNum").child("Place").setValue(["Timein": "15:10:10", "Timeout": "15:30:10", "Price": "0", "Place": "สยามพารากอน"])
+        for i in 1...10 {
+            self.ref.child("AccountNum").child("PlateNum").child("Bill\(i)").setValue(["Timein": "15:10:10", "Timeout": datetime1, "Price": "10", "Place": "สยามพารากอน"])
+        }
+            
+            //let PlateNum = Auth.auth().currentUser?.uid
+        //    ref.child("AccountNum").child("PlateNum").child("Place").child("Time").observeSingleEvent(of: .value, with: { (snapshot) in
+        //      // Get user value
+        //      let value = snapshot.value as? NSDictionary
+        //      let price = value?["Price"] as? String ?? ""
+        //      //let user = User(username: username)
+        //        print("value \(value)")
+        //        print("price \(price)")
+        //      // ...
+        //      }) { (error) in
+        //        print(error.localizedDescription)
+        //    }
     }
+    
+    
+    
     
 
     /*
